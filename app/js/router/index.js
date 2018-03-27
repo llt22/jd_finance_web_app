a6d1dd58c796aea89d93fa1b2ad9f414d82ac8e7
llt22 <262387013@qq.com>
2018-03-28 03:48:48 +0800

    众筹相关组件编写

--- app/js/router/index.js
+++ app/js/router/index.js
@@ -3,6 +3,7 @@ import Router from "vue-router"
 import Home from "../home/index.vue"
 import Money from '../money/index.vue'
 import Ious from '../ious/index'
+import Raise from '../raise/index'
 
 Vue.use(Router)
 
@@ -26,6 +27,11 @@ export default new Router({
             path: "/ious",
             name: "ious",
             component: Ious,
+        },
+        {
+            path: "/raise",
+            name: "raise",
+            component: Raise,
         },
     ],
 })
